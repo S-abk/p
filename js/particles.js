38188307881efd109aa7a1440b431a937c2c8224
S-abk <117982032+S-abk@users.noreply.github.com>
2024-06-20 16:53:35 +0100

    Update particles.js

--- js/particles.js
+++ js/particles.js
@@ -673,37 +673,44 @@ var pJS = function(tag_id, params){
 
   /* ---------- pJS functions - particles interaction ------------ */
 
-  pJS.fn.interact.linkParticles = function(p1, p2){
+pJS.fn.interact.linkParticles = function(p1, p2){
 
-    var dx = p1.x - p2.x,
-        dy = p1.y - p2.y,
-        dist = Math.sqrt(dx*dx + dy*dy);
+  var dx = p1.x - p2.x,
+      dy = p1.y - p2.y,
+      dist = Math.sqrt(dx*dx + dy*dy);
 
-    /* draw a line between p1 and p2 if the distance between them is under the config distance */
-    if(dist <= pJS.particles.line_linked.distance){
+  /* draw a line between p1 and p2 if the distance between them is under the config distance */
+  if(dist <= pJS.particles.line_linked.distance){
 
-      var opacity_line = pJS.particles.line_linked.opacity - (dist / (1/pJS.particles.line_linked.opacity)) / pJS.particles.line_linked.distance;
+    var opacity_line = pJS.particles.line_linked.opacity - (dist / (1/pJS.particles.line_linked.opacity)) / pJS.particles.line_linked.distance;
 
-      if(opacity_line > 0){        
-        
-        /* style */
-        var color_line = pJS.particles.line_linked.color_rgb_line;
-        pJS.canvas.ctx.strokeStyle = 'rgba('+color_line.r+','+color_line.g+','+color_line.b+','+opacity_line+')';
-        pJS.canvas.ctx.lineWidth = pJS.particles.line_linked.width;
-        //pJS.canvas.ctx.lineCap = 'round'; /* performance issue */
-        
-        /* path */
-        pJS.canvas.ctx.beginPath();
-        pJS.canvas.ctx.moveTo(p1.x, p1.y);
-        pJS.canvas.ctx.lineTo(p2.x, p2.y);
-        pJS.canvas.ctx.stroke();
-        pJS.canvas.ctx.closePath();
+    if(opacity_line > 0){        
 
-      }
+      /* Calculate angle */
+      var angle = Math.atan2(dy, dx);
+
+      /* Calculate thickness based on angle */
+      var thickness = Math.abs(Math.cos(angle)) * pJS.particles.line_linked.width;
+
+      /* style */
+      var color_line = pJS.particles.line_linked.color_rgb_line;
+      pJS.canvas.ctx.strokeStyle = 'rgba('+color_line.r+','+color_line.g+','+color_line.b+','+opacity_line+')';
+      pJS.canvas.ctx.lineWidth = thickness;
+      //pJS.canvas.ctx.lineCap = 'round'; /* performance issue */
+      
+      /* path */
+      pJS.canvas.ctx.beginPath();
+      pJS.canvas.ctx.moveTo(p1.x, p1.y);
+      pJS.canvas.ctx.lineTo(p2.x, p2.y);
+      pJS.canvas.ctx.stroke();
+      pJS.canvas.ctx.closePath();
 
     }
 
-  };
+  }
+
+};
+
 
 
   pJS.fn.interact.attractParticles  = function(p1, p2){
